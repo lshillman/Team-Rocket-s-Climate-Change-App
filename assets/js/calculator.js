2044705badb280db40ba416a2830341c73703729
Luke <lshillman@gmail.com>
2022-07-07 20:55:37 -0700

    parse response from world bank api

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,8 +1,23 @@
 console.log("I'm a JavaScript file linked to this CALCULATOR page!");
 
 var countryEl = $('#country');
-var country;
+var country = "USA";
+var GDP;
+var wbURL1 = 'http://api.worldbank.org/v2/country/'
+var wbURL2 = '/indicator/NY.GDP.MKTP.CD?date=2021:2021&format=json'
+var testURL = 'http://api.worldbank.org/v2/country/BRA/indicator/NY.GDP.MKTP.CD?date=2021:2021&format=json'
 
-function collectCountry
+function collectCountry () {
+    country = countryEl.val()
+}
 
-function getGDP(country)
+function getGDP() {
+    fetch(wbURL1 + countryEl.val() + wbURL2)
+    // fetch(testURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data){
+        console.log(data);
+      })
+}

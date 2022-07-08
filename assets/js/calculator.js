07974d70c048bbc81f9a38447df1a767786656d8
lshillman <lshillman@users.noreply.github.com>
2022-07-08 12:11:08 -0700

    add autocomplete js and css files, and MAKE IT WORK ON THE PAGE

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -7,6 +7,8 @@ var wbURL1 = 'http://api.worldbank.org/v2/country/'
 var wbURL2 = '/indicator/NY.GDP.MKTP.CD?date=2021:2021&format=json'
 var testURL = 'http://api.worldbank.org/v2/country/BRA/indicator/NY.GDP.MKTP.CD?date=2021:2021&format=json'
 
+var countries = ['France', 'Russia', 'United States', 'United Kingdom', 'Bahamas', 'Bermuda', 'Russia']; // we'll get this from the restcountries api
+
 function collectCountry () {
     country = countryEl.val()
 }
@@ -20,8 +22,16 @@ function getGDP() {
         return response.json();
       })
       .then(function (data){
-        // console.log(data);
+        console.log(data);
         GDP = data[1][0].value;
         console.log(GDP);
       })
 }
+
+
+
+accessibleAutocomplete({
+  element: document.querySelector('#countryAutocomplete'),
+  id: 'countryAutocomplete', // To match it to the existing <label>.
+  source: countries
+})

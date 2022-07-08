71b7347483c4b6c7374fac58b7279d19a6c38acf
lshillman <lshillman@users.noreply.github.com>
2022-07-08 12:36:39 -0700

    fetch countries from country API and put results into autocomplete

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -3,11 +3,12 @@ console.log("I'm a JavaScript file linked to this CALCULATOR page!");
 var countryEl = $('#country');
 var country = "USA";
 var GDP;
-var wbURL1 = 'http://api.worldbank.org/v2/country/'
-var wbURL2 = '/indicator/NY.GDP.MKTP.CD?date=2021:2021&format=json'
-var testURL = 'http://api.worldbank.org/v2/country/BRA/indicator/NY.GDP.MKTP.CD?date=2021:2021&format=json'
+var wbURL1 = 'http://api.worldbank.org/v2/country/';
+var wbURL2 = '/indicator/NY.GDP.MKTP.CD?date=2021:2021&format=json';
+var testURL = 'http://api.worldbank.org/v2/country/BRA/indicator/NY.GDP.MKTP.CD?date=2021:2021&format=json';
 
-var countries = ['France', 'Russia', 'United States', 'United Kingdom', 'Bahamas', 'Bermuda', 'Russia']; // we'll get this from the restcountries api
+var countries = [];
+//['France', 'Russia', 'United States', 'United Kingdom', 'Bahamas', 'Bermuda', 'Russia']; // we'll get this from the restcountries api
 
 function collectCountry () {
     country = countryEl.val()
@@ -28,6 +29,23 @@ function getGDP() {
       })
 }
 
+
+function getCountries () {
+  fetch('https://restcountries.com/v3.1/all?fields=name,cca3')
+  // fetch(testURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+      console.log(data);
+
+      for (i=0; i < data.length; i++) {
+        countries.push(data[i].name.common + " - " + data[i].cca3)
+      }
+      countries.sort();
+      console.log(countries);
+    })
+}
 
 
 accessibleAutocomplete({

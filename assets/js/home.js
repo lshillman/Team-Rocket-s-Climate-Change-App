00dd263b143ab61a4d77021c1f70f298ea1b237d
MiloCOLO <100871599+MiloCOLO@users.noreply.github.com>
2022-07-10 17:47:02 -0600

    linked climate data api and displayed on main homepage

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,22 @@
 console.log("I'm a JavaScript file linked to this page!");
+var fetchBtn = document.getElementById("government")
+
+function getFucked() {
+    console.log("I'm in the getFucked function");
+    var iceData = 'https://global-warming.org/api/arctic-api';
+    const element = document.getElementById('ice-level');
+
+    fetch(iceData)
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (data){
+            console.log(data.arcticData[data.arcticData.length-1].extent)
+            element.innerText = (data.arcticData[data.arcticData.length-1].extent + " million km^2");
+    })
+
+}
+
+// fetchBtn.addEventListener('click', getFucked)
+
+getFucked();
